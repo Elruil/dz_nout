7b8620a4d97327900a21e40f32ca2c7f9c03659d
elruil <elandriel@mail.ru>
2023-08-10 11:41:28 +0500

    editing example023

--- example023/Program.cs
+++ example023/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 23: Напишите программу, которая принимает на
 //вход число (N) и выдаёт таблицу кубов чисел от 1 до N..
-int num = GetNum("Введите число таблицу кубов которого хотите получить", "Ошибка ввода данных");
+int num = GetNum("Введите число таблицу кубов которого хотите получить", "Ошибка ввода данных", "Введите положительное число больше 0");
 Console.WriteLine($"Введенное вами число {num}, таблицей кубов которго являются");
 
 for (int i = 1; i <= num; i++)
@@ -9,7 +9,7 @@ for (int i = 1; i <= num; i++)
     Console.Write($" {result},");
     
 }
-static int GetNum(string Message, string errorMessage)
+static int GetNum(string Message, string errorMessage, string ErrorEnter)
 {
     while (true)
     {
@@ -17,7 +17,9 @@ static int GetNum(string Message, string errorMessage)
         {
             Console.WriteLine(Message);
             int num = int.Parse(Console.ReadLine() ?? "");
+            if (num > 0)
             return num;
+            else Console.WriteLine(ErrorEnter);
         }
         catch (Exception exc)
         {
