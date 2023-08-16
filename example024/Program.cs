92922aded17d26ad5989fff063a465979c3a83ca
elruil <elandriel@mail.ru>
2023-08-16 12:40:47 +0500

    adding example 024-027

---
+++ example024/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 24: Напишите программу, которая
+//принимает на вход число (А) и выдаёт сумму
+//чисел от 1 до А
+//4 -> 10 (1+2+3+4=10)
+//7 -> 28
+//8 -> 36
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число А", "Ошибка ввода");
+int sumNumbers = GetSumNumbers(num);
+Console.WriteLine($"{num} -> {sumNumbers}");
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+        while(number > 0)
+        {
+            sum += number;
+            number--;
+        }
+        return sum;
+}
+int GetNumberFromUser(string Message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(Message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
---
+++ example027/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Clear();
+int number = GetNum("Введите число, сумму чисел которого вы хотите узнать", "Ошибка ввода", "Введите число больше нуля");
+int summ = GetSumNumbers(number);
+static int GetNum(string Message, string errorMessage, string ErrorEnter)
+{
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine(Message);
+            int num = int.Parse(Console.ReadLine() ?? "");
+            if (num > 0)
+                return num;
+            else Console.WriteLine(ErrorEnter);
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"{errorMessage} {exc.Message}");
+        }
+    }
+}
+int GetSumNumbers(int number)
+{   int sum = 0;
+    while (number > 0)
+
+    {
+        int a = number % 10;
+        int b = number / 10;
+        sum = +a;
+    }
+    return sum;
+}
+Console.WriteLine($"{number}    {summ}");
